f760512e6c8f1a5a9b23774a310a49bb8d3848e8
lillipilli6 <40557831+lillipilli6@users.noreply.github.com>
2021-04-01 18:28:34 +1100

    Changes to bootstrap

---
+++ js/index.js
@@ -0,0 +1,12 @@
+
+$("#linkClicked").on("click", function() {
+    alert( "Here is an alert!" );
+  });
+
+
+  
+  $(document).ready(function(){
+    $("#uxShow").click(function(){
+      $("ul").toggle();
+    });
+  });
